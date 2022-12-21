b3fc9ec33cd19e91ae6539cf3fc0c2a66d524126
Martin Krajcovic <mar.krajc@gmail.com>
2022-12-21 22:14:34 +0100

    added cli parsing + validation

--- src/main/java/com/learning/core/AppRunner.java
+++ src/main/java/com/learning/core/AppRunner.java
@@ -26,23 +26,8 @@ public class AppRunner {
 	private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) {
-		Options options = new Options();
-
-		Option input = new Option("f", "file", true, "output file path");
-        input.setRequired(false);
-        options.addOption(input);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
-        try {
-           cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            println(e.getMessage());
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("utility-name", options);
-            System.exit(1);
-        }
-
+		validateCommandLineArguments(args);
+		CommandLine cmd = parseCommandLineArguments(args);
 		try {
 			BufferedImage image = QRWifi.generateWifiQRCode(args[0], args[1]);
 			String optionValue = cmd.getOptionValue("file", "display");
@@ -53,10 +38,38 @@ public class AppRunner {
 				break;
 			default:
 				saveImage(image, new File(optionValue));
+				println("Image has been saved to: " + optionValue);
 			}
 		} catch (WriterException | IOException e) {
 			println(e.getMessage());
 		}
+	}
+
+	private static void validateCommandLineArguments(String[] args) {
+		if (args.length < 2) {
+			println("SSID/Name and password arguments are mandatory");
+			System.exit(1);
+		}
+	}
+
+	private static CommandLine parseCommandLineArguments(String[] args) {
+		Options options = new Options();
+
+		Option input = new Option("f", "file", true, "output file path");
+		input.setRequired(false);
+		options.addOption(input);
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			println(e.getMessage());
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("utility-name", options);
+			System.exit(1);
+		}
+		return cmd;
 	}
 
 	@SuppressWarnings("serial")
